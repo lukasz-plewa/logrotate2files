89c88868c477a4eb0e58fe9aa15849eed1ac28af
Lukasz Plewa <L.Plewa@adbglobal.com>
2016-03-04 10:28:56 +0100

    Publish log via http page on 8080 port

--- src/logrotate2files.c
+++ src/logrotate2files.c
@@ -12,11 +12,14 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
-#include <sys/stat.h>
 #include <time.h>
-#include <semaphore.h>
+#include<errno.h>
+#include<arpa/inet.h>
+#include<sys/socket.h>
+#include<fcntl.h>
+#include<string.h>
 
-#define LOG_SIZE_LIMIT    256
+#define LOG_SIZE_LIMIT    (1024 * 5)
 #define LOG_FILES_CNT_MAX 8
 #define LOG_FILENAME_LEN  16
 
@@ -39,6 +42,7 @@ struct test_container {
 static int initLog(struct log_rotate_file *log, int files_cnt);
 static int putLineToLog(const char *line, ssize_t len, struct log_rotate_file *log);
 static void log_destroy(struct log_rotate_file *log);
+static void send_log_as_http11chunks(int sockfd, struct log_rotate_file *log);
 void* WriterThread(void *arg);
 void* ReaderThread(void *arg);
 void print_log_to_file(const char *name, struct log_rotate_file *log);
@@ -56,7 +60,7 @@ void generate_input_file(const char *name)
     return;
   }
   srand(time(NULL));
-  for(i = 0; i < 100000; i++)
+  for(i = 0; i < LOG_SIZE_LIMIT; i++)
   {
     snprintf(buf, sizeof(buf), "%d-Testowa linijka numer %d\n", i, (rand() % 10000));
     fputs(buf, fd);
@@ -80,13 +84,13 @@ int main(int argc, char **argv)
 	  return EXIT_FAILURE;
 	}
 
-	if (initLog(&log, 5) != EXIT_SUCCESS)
+	if (initLog(&log, 8) != EXIT_SUCCESS)
 	{
 	  printf("Error initializing LOG file\n");
 	  return EXIT_FAILURE;
 	}
-	//  generate_input_file(argv[1]);
-	//  return 0;
+//	  generate_input_file(argv[1]);
+//	  return 0;
 	container.log = &log;
 	container.input_file_name = argv[1];
 
@@ -114,26 +118,76 @@ int main(int argc, char **argv)
 	return EXIT_SUCCESS;
 }
 
+
 void* ReaderThread(void *arg)
 {
   struct test_container *container = (struct test_container *)arg;
   struct log_rotate_file *log = container->log;
 
+  struct sockaddr_in sockserv, sockclient;
+  int socketfd, clientfd;
+  socklen_t clientsocklen;
+  int finalcnt = 0;
+
+  char buff[BUFSIZ];
+
+  socketfd = socket(AF_INET,SOCK_STREAM,0);
+  printf("Socket Creation: %s\n", strerror(errno));
+
+  bzero(&sockserv,sizeof(sockserv));
+
+  sockserv.sin_family = AF_INET;
+  sockserv.sin_addr.s_addr = INADDR_ANY;
+  sockserv.sin_port = htons(8080);
+
+  bind(socketfd,(struct sockaddr *)&sockserv, sizeof(sockserv));
+  printf("Socket Bind: %s\n", strerror(errno));
+
+  listen(socketfd,10);
+  printf("Socket Listen: %s\n",strerror(errno));
+
+  sleep(2);
+
   do
   {
+    clientfd = accept(socketfd, (struct sockaddr*)&sockclient, &clientsocklen);
+    printf("request accepted\n");
+    buff[read(clientfd, buff, BUFSIZ)] = '\0';
+    printf("Request recieved as \n%s\n",buff);
+
+    snprintf(buff, BUFSIZ, "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nDate: Mon, 28 Feb 2011 10:38:19 GMT\r\nTransfer-Encoding: chunked\r\nServer: Myserver\r\n\r\n%x\r\n%s\r\n",(unsigned int)strlen("<html><body><p> HI I am anirudh tomer </p><br> <pre>"),"<html><body><p> HI I am anirudh tomer </p><br> <pre>");
+    printf("\n\nREPLY TO THE CLIENT\n\n%s\n\n",buff);
+    finalcnt = send(clientfd, buff, strlen(buff), 0);
+    printf("Sent %d bytes to the client : %s\n", finalcnt, strerror(errno));
+
     pthread_mutex_lock(&log->mutex_log);
-    printf("Reader obtained semaphore\n");
     printf("Log size is:     %d\n", (int)ftell(log->logfile[log->current]));
     printf("Log position:    %d\n", log->file_pos[log->current]);
     printf("Log current file %d\n", log->current);
-
-
-    print_log_to_file("output.log", log);
-
+    printf("Reader obtained semaphore\n");
+    //    print_log_to_file("output.log", log);
+    if (log->file_pos[log->current] > 0)
+    {
+      send_log_as_http11chunks(clientfd, log);
+    }
+    else
+    {
+      finalcnt = snprintf(buff, BUFSIZ, "%x\r\nLog is empty!!!\nSorry.0\r\n\r\n", (unsigned int)strlen("Log is empty!!!\nSorry."));
+      finalcnt = send(clientfd, buff, finalcnt, 0);
+    }
     pthread_mutex_unlock(&log->mutex_log);
-    printf("Reader released semaphore\n");
-    usleep(1000000);
+
+    finalcnt = snprintf(buff, BUFSIZ, "%x\r\n</pre> </body></html>0\r\n\r\n", (unsigned int)strlen("</pre> </body></html>"));
+    finalcnt = send(clientfd, buff, finalcnt, 0);
+
+    printf("\n\nREPLY TO THE CLIENT\n\n%s\n\n",buff);
+    usleep(10000);
+    close(clientfd);
+
   } while(1);
+
+
+  close(socketfd);
 }
 
 void* WriterThread(void *arg)
@@ -159,8 +213,8 @@ void* WriterThread(void *arg)
   else
     line_len = 1024;
   printf("%s thread started. Line buf %d length allocated\n", __FUNCTION__, (int)line_len);
-  printf("Container consist of:\nFilename: %s\n", container->input_file_name);
-
+  printf("Input file name: %s\n", container->input_file_name);
+  sleep(10);
   do
   {
     for( i=0; i < 2 && read_size >= 0; i++)
@@ -172,8 +226,8 @@ void* WriterThread(void *arg)
         continue;
       }
       putLineToLog(line, read_size, log);
+      usleep(100000);
     }
-    sleep(1);
   } while(read_size >= 0);
 
   printf("Ending Writer Thread\n");
@@ -181,6 +235,58 @@ void* WriterThread(void *arg)
   fclose(finput);
 
   return NULL;
+}
+
+/* *****************************************************************************
+ *
+ * ****************************************************************************/
+static void send_log_as_http11chunks(int sockfd, struct log_rotate_file *log)
+{
+  char *line = NULL;
+  size_t line_len = 0;
+  int cur = log->current;           // index of currently written log file
+  int itr = cur;                    // index for reading files
+  ssize_t read_len;
+  unsigned int finalcnt;
+  char header[256];
+
+  line = malloc(1024);
+  if (line == NULL)
+    line_len = 0;
+  else
+    line_len = 1024;
+
+  do
+  {
+    itr = ((itr + 1) % log->files_cnt);
+    if (log->file_full[itr] || log->current == itr)   /* skip empty files but print current*/
+    {
+      printf("Printing file %d\n", itr);
+      fseek(log->logfile[itr], 0, SEEK_SET);
+      while (1)
+      {
+        read_len = getline(&line, &line_len, log->logfile[itr]);
+        if (read_len > 0)
+        {
+          snprintf(header, sizeof(header), "%x\r\n", (unsigned int)read_len);
+          finalcnt = send(sockfd, header, strlen(header), 0);
+          finalcnt += send(sockfd, line, (size_t)read_len, 0);
+          finalcnt += send(sockfd, "\r\n", 2, 0);
+          printf("Response: %s %s\n", header, line);
+          printf("Sent %d bytes to the client : %s\n", finalcnt, strerror(errno));
+          continue;
+        }
+        printf("Printing file finished\n");
+        break;
+      }
+    }
+    else
+    {
+      printf("Skipping %d file\n", itr);
+    }
+  }while (itr != cur);
+
+  free(line);
 }
 
 /* *****************************************************************************
@@ -240,16 +346,16 @@ static int putLineToLog(const char *line, ssize_t len, struct log_rotate_file *l
   }
   pthread_mutex_lock(&log->mutex_log);
   cur = log->current;
-  printf("Inserting line to file %d at pos %d\n", cur, log->file_pos[cur]);
-  sleep(1);
+//  printf("Inserting line to file %d at pos %d\n", cur, log->file_pos[cur]);
+  usleep(100000);
   fseek(log->logfile[cur], log->file_pos[cur], SEEK_SET);
   fwrite(line, len, 1, log->logfile[cur]);
   log->file_pos[cur] = ftell(log->logfile[cur]);
-  printf("Inserted %d bytes\n", (int)len);
+//  printf("Inserted %d bytes\n", (int)len);
 
   if (log->file_pos[cur] > LOG_SIZE_LIMIT)
   {
-    printf("Size limit %d reached in file %d, rotating file.\n", LOG_SIZE_LIMIT, cur);
+//    printf("Size limit %d reached in file %d, rotating file.\n", LOG_SIZE_LIMIT, cur);
     fflush(log->logfile[cur]);
     log->file_full[cur] = 1;
     cur = ((cur + 1) % log->files_cnt);
